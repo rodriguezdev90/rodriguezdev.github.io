8c7fdc5e18b6eac3c83a70d04b9f6eec86c730da
rodriguezdev90 <teacher.rodriguez90@gmail.com>
2024-09-07 23:41:09 -0600

    Add files via upload

---
+++ proyectos/calc/script.js
@@ -0,0 +1,140 @@
+/* Variables para el login */
+
+let firstName = prompt("Por favor ingresa tu nombre: ");
+let greeting = "¡Bienvenido ";
+let pregunta = "¿Qué operación deseas realizar, ";
+let result = firstName.bold();
+
+document.getElementById("saludos").innerHTML = greeting + result + ", te has conectado!";
+document.getElementById("pregunta").innerHTML = pregunta + result + "?";
+
+/* Variables para la calculadora */
+
+var datoa;
+var datob;
+var operacion;
+
+/* Función init y sus respectivas variables */
+
+function init(){
+    
+    var resultado = document.getElementById('resultado');
+    var reset = document.getElementById('reset');
+    var suma = document.getElementById('suma');
+    var resta = document.getElementById('resta');
+    var multiplicacion = document.getElementById('multiplicacion');
+    var division = document.getElementById('division');
+    var igual = document.getElementById('igual');
+    var uno = document.getElementById('uno');
+    var dos = document.getElementById('dos');
+    var tres = document.getElementById('tres');
+    var cuatro = document.getElementById('cuatro');
+    var cinco = document.getElementById('cinco');
+    var seis = document.getElementById('seis');
+    var siete = document.getElementById('siete');
+    var ocho = document.getElementById('ocho');
+    var nueve = document.getElementById('nueve');
+    var cero = document.getElementById('cero');
+    }
+
+/* Eventos de click de los botones */
+
+uno.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "1";
+}
+dos.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "2";
+}
+tres.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "3";
+}
+cuatro.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "4";
+}
+cinco.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "5";
+}
+seis.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "6";
+}
+siete.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "7";
+}
+ocho.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "8";
+}
+nueve.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "9";
+}
+cero.onclick = function(e){
+    resultado.textContent = resultado.textContent  + "0";
+}
+reset.onclick = function(e){
+    resetear();
+}
+suma.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "+";
+    limpiar();
+}
+resta.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "-";
+    limpiar();
+}
+multiplicacion.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "*";
+    limpiar();
+}
+division.onclick = function(e){
+    operandoa = resultado.textContent;
+    operacion = "/";
+    limpiar();
+}
+igual.onclick = function(e){
+    operandob = resultado.textContent;
+    resolver();
+}
+
+/* La función para limpiar las variables que tienen un número asignado */
+
+function limpiar(){
+    resultado.textContent = "";
+  }
+
+/* La función para resetear todos los datos */
+
+  function resetear(){
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+  }
+
+  /* Función que se encarga de realizar la operación en base a los datos previamente ingresados */
+
+  function resolver(){
+    var res = 0;
+    switch(operacion){
+      case "+":
+        res = parseFloat(operandoa) + parseFloat(operandob);
+        break;
+      case "-":
+          res = parseFloat(operandoa) - parseFloat(operandob);
+          break;
+      case "*":
+        res = parseFloat(operandoa) * parseFloat(operandob);
+        break;
+      case "/":
+        res = parseFloat(operandoa) / parseFloat(operandob);
+        break;
+    }
+    resetear();
+    resultado.textContent = res;
+  }
+
+
+
+
+
